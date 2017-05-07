607e0c419a3b488e9799be778f6c0bb904da60e4
Tony Karanja <fenixpiconi@gmail.com>
2017-05-06 19:22:30 -0500

    edited conversionns.js file 'Invalid Input' option still prints a result..

--- conversions.js
+++ conversions.js
@@ -1,35 +1,33 @@
 var rates = [0.0127, 75.6222, 0.0145, 69.0159, 1.1388, 0.8781];
 
-var RUDEUR = rates[0];
+var RUBEUR = rates[0];
 var EURRUB = rates[1];
 var RUBUSD = rates[2];
 var USDRUB = rates[3];
 var EURUSD = rates[4];
 var USDEUR = rates[5];
 
-const port = 8008
-
 var result = 0;
 
 function convert(amount, convertFrom, convertTo) {
-	if (convertFrom == "RUB" && convertTO =="EUR") {
-		reulst = amount = RUBEUR;
+	if (convertFrom == "RUB" && convertTo =="EUR") {
+		result = amount * RUBEUR;
 	} else if (convertFrom == "EUR" && convertTo == "RUB") {
-		result = amount = EURRUB;	
+		result = amount * EURRUB;
 	} else if (convertFrom == "RUB" && convertTo == "USD") {
-		result = amount = RUBUSD;
+		result = amount * RUBUSD;
 	} else if (convertFrom == "USD" && convertTo == "RUB") {
-		result = amount = USDRUB;
+		result = amount * USDRUB;
 	} else if (convertFrom == "EUR" && convertTo == "USD") {
-		result = amount = EURUSD;
+		result = amount * EURUSD;
 	} else if (convertFrom == "USD" && convertTo == "EUR") {
-		result = amount = USEEUR;
+		result = amount * USEEUR;
 	} else if (convertFrom === convertTo) {
 		result = amount;
 	} else {
-		alert ("Invalid Input");
+		console.log("Invalid Input");
 	}
-	document.write(amount + " " + convertFrom + " = " + result + " " + convertTo + "<br>");
+	console.log(amount + " " + convertFrom + " = " + result + " " + convertTo);
 }
 
 convert(100, "USD", "RUB");
